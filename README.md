f939322ff062f910e8e921749a93f7a4ef2a4e44
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2022-11-18 19:03:39 +0600

    blog & readme done

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-client site[live website](https://learntocode-88f5e.web.app)
+LearnToCode[live website](https://learntocode-88f5e.web.app)
+
+### About the site
+
+- The site is all about learning courses
+- There are 6 courses available you have to select one to view the details
+- You have to login or signup for getting premium access
+- You can login with google, github and also email.
+- Unknown route will take you to 404 page
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,12 +1,62 @@
-import React from 'react';
+import React from "react";
+import {
+  FaFacebook,
+  FaGooglePlus,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>
-      <h2>this is footer</h2>
-      <p>
-        <small>copyright coming soon</small>
-      </p>
+    <div className="mx-auto text-center text-white bg-dark footer">
+      <div className="footer-left col-md-4 col-sm-6">
+        <p className="about">
+          <span> About the company</span> providing you the best learning
+          experience.
+        </p>
+        <div className="icons p-2 d-flex">
+          <div className=" p-2">
+            <FaFacebook></FaFacebook>
+          </div>
+          <div className=" p-2">
+            <FaTwitter></FaTwitter>
+          </div>
+          <div className=" p-2">
+            <FaInstagram></FaInstagram>
+          </div>
+          <div className=" p-2">
+            <FaGooglePlus></FaGooglePlus>
+          </div>
+        </div>
+      </div>
+      <div className="footer-center col-md-4 col-sm-6">
+        <div>
+          <i className="fa fa-map-marker"></i>
+          <p>
+            <span> Shaheed Sharani, Dhaka 1206</span> Dhaka, Bangladesh
+          </p>
+        </div>
+        <div>
+          <i className="fa fa-phone"></i>
+          <p> (+00) 0000 000 000</p>
+        </div>
+        <div>
+          <i className="fa fa-envelope"></i>
+          <p>
+            <> office@company.com</>
+          </p>
+        </div>
+      </div>
+      <div className="footer-right col-md-4 col-sm-6">
+        <h2>
+          Learn<span>toCode</span>
+        </h2>
+        <p className="menu">
+          <> Home</> |<> About</> |<> Services</>
+        </p>
+        <p className="name"> LearnToCode &copy; 2022</p>
+      </div>
     </div>
   );
 };
